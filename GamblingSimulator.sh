b0cc030161fe0802fdfd30fea07d242ec30ff7e8
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-07-11 14:40:52 +0530

    [Pradip] Added:The result will be declared after twenty days

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,18 +4,29 @@ STAKE=100
 BET=1
 MAX_PROFIT_PER_DAY=50
 MAX_LOSS_PER_DAY=-50
-wallet=0
-while [[ $wallet -lt $MAX_PROFIT_PER_DAY && $wallet -gt $MAX_LOSS_PER_DAY ]]
+count=0
+totalAmount=100
+for (( day=1; day<=10; day++))
 do
-	betCheck=$((RANDOM%2))
-	if [ $WIN -eq $betCheck ]
-	then
-		wallet=$(($wallet+$BET))
-	else
-		wallet=$(($wallet-$BET))
-	fi
+	wallet=0
+	while [[ $wallet -lt $MAX_PROFIT_PER_DAY && $wallet -gt $MAX_LOSS_PER_DAY ]]
+	do
+		betCheck=$((RANDOM%2))
+		if [ $WIN -eq $betCheck ]
+		then
+			wallet=$(($wallet+$BET))
+		else
+			wallet=$(($wallet-$BET))
+		fi
+	done
+echo $wallet
+dayResult[((count++))]=$wallet
+totalAmount=$(($totalAmount+$wallet))
 done
-
-echo "$wallet"
-echo "wallet amout =$(($STAKE+$wallet))"
-
+echo "result for every day ${dayResult[@]}"
+if (( $totalAmount >= 0 ))
+then
+	echo "Total profit $totalAmount"
+else
+	echo "Total loose amoutn $totalAmount"
+fi
